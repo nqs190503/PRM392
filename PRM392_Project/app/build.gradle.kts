f2b47f047e4fabf2e1e9d1f4c57f450071f64223
Su Nguyen <nqs190503@gmail.com>
2025-03-23 22:51:13 +0700

    change layout payment

--- PRM392_Project/app/build.gradle.kts
+++ PRM392_Project/app/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment:2.7.4")
     implementation("androidx.navigation:navigation-ui:2.7.4")
     implementation("androidx.activity:activity:1.10.1")
+    implementation("com.squareup.picasso:picasso:2.8")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- PRM392_Project/app/src/main/java/com/prm3/onlineshopping/OrderActivity.java
+++ PRM392_Project/app/src/main/java/com/prm3/onlineshopping/OrderActivity.java
@@ -1,12 +1,15 @@
 package com.prm3.onlineshopping;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.squareup.picasso.Picasso;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -20,5 +23,12 @@ public class OrderActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        ImageView imageView = findViewById(R.id.imgQR);
+        String url = "https://scontent.fhan15-2.fna.fbcdn.net/v/t1.15752-9/462578759_986587626409565_7330512181133743099_n.jpg?stp=dst-jpg_s1080x2048_tt6&_nc_cat=104&ccb=1-7&_nc_sid=9f807c&_nc_eui2=AeGUKru0rMTwkDpJDWQE94wyNHDRJMhOXb40cNEkyE5dvqx8JCtKDzNrJ3psf9nSIp_VTkvz4UWXSYXr-mwAt1DP&_nc_ohc=9BkCAQBsS_cQ7kNvgHSVK1V&_nc_oc=AdlWYkGQzu92cnSGdgosaXtr9c4Cu5h4VgRN-Qfjx6YRYZGk_5QEIwvBDxWMxGaPDV9PLtrKtfB3tajAh8u3aucl&_nc_zt=23&_nc_ht=scontent.fhan15-2.fna&oh=03_Q7cD1wHxCGHLzj31Wi_roWJcBoJ-h6K9mWeWyFlLrKGlP4nL_Q&oe=6807B0BF";
+        Picasso.get()
+                .load(url)
+                .placeholder(R.drawable.baseline_account_circle_24) // Ảnh hiển thị khi đang tải
+                .error(R.drawable.baseline_error_24) // Ảnh hiển thị nếu lỗi
+                .into(imageView);
     }
 }
